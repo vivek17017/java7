6c2bb5041fee3523231ba65f423abd530f2020be
Vivek Kumar <vivekgupta70509@gmail.com>
2025-07-11 15:48:35 +0530

    Add files via upload

---
+++ Assignment7.java
@@ -0,0 +1,74 @@
+class Complex{
+    int real,imag;
+    void setComplexNumber(int r,int i)
+    {
+        real=r;
+        imag=i;
+    }
+    void printValues()
+    {
+        System.out.println("real="+real+" imaginary="+imag);
+    }
+}
+class Time{
+    int hr,min,sec;
+    void setTime(int h,int m,int s)
+    {
+        hr=h;
+        min=m;
+        sec=s;
+    }
+    void showTime()
+    {
+        System.out.println(hr+" hr "+min+" min "+sec+" sec");
+    }
+}
+class Cuboid{
+    int length,breadth,height;
+    void setLength(int l){
+        length=l;
+    }
+    void setBreadth(int b){
+        breadth=b;
+    }
+    void setHeight(int h){
+        height=h;
+    }
+    int getLength(){
+        return length;
+    }
+    int getBreadth(){
+        return breadth;
+    }
+    int getHeight(){
+        return height;
+    }
+    int calculateVolume(){
+        return (length*breadth*height);
+    }
+    int calculateSurfaceArea(){
+        return (2*(length*breadth+breadth*height+length*height));
+    }
+}
+class Employee{
+    int empid;
+    char name[50];
+    float salary;
+    void setEmpid(int e){empid=e;}
+
+    void setName(char[] name) {
+        this.name = name;
+    }
+    void setSalary(float s){salary=s;}
+    int getEmpid(){return empid;}
+    float getSalary(){return salary;}
+
+}
+public class Assignment7 {
+    public static void main(String[]args)
+    {
+        Time c=new Time();
+        c.setTime(4,5,5);
+        c.showTime();
+    }
+}
